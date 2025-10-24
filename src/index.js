e374e55e15c24fae672215b44eb228682db70ede
Gaurab-Moharana <gauravmoharana8560@gmail.com>
2025-10-24 23:01:43 +0530

    cron added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import job from "./lib/cron.js";
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
+job.start();
 app.use(express.json());
 app.use(cors());
 app.use("/api/auth", authRoutes);
